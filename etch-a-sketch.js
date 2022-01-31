52b0a94f05696ec3e03aa49401225363c193c6b5
Aza Walker <aza.walkerjr@relativity.com>
2022-01-31 14:59:30 -0500

    Game complete

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,6 +1,5 @@
 const GRID_WIDTH = 600;
 const gameContainer = document.querySelector('.game');
-//const grid = document.querySelector('.grid');
 const colorPicker = document.getElementById('color');
 const normalButton = document.getElementById('normal')
 const rainbowButton = document.getElementById('rainbow')
@@ -8,21 +7,31 @@ const eraserButton = document.getElementById('eraser')
 const clearButton = document.getElementById('clear')
 const slider = document.getElementById('slider')
 const sliderText = document.querySelector('.grid-size')
-
+let gridSize = slider.value;
+let grid;
 
 function makeSquare(grid, gridSize){
     const square = document.createElement('div');
     square.classList.add('square');
     square.style.width = GRID_WIDTH/gridSize + 'px';
     square.style.height = GRID_WIDTH/gridSize + 'px';
-    square.addEventListener('mouseover', () => {
-        square.style.backgroundColor = colorPicker.value;
-    });
+    if(rainbowButton.classList == 'active'){
+        square.addEventListener('mouseover', () => {
+            let randomColor = Math.floor(Math.random()*16777215).toString(16);
+            square.style.backgroundColor = '#' + randomColor;
+        });
+    }
+    else { //normal
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = colorPicker.value;
+        });
+    }
     grid.appendChild(square)
 }
 
 function makeGrid(gridSize){
     const grid = document.createElement('div');
+    this.grid = grid 
     grid.classList.add('grid')
     grid.style.width = GRID_WIDTH + 'px';
     gameContainer.appendChild(grid);
@@ -36,14 +45,51 @@ function unmakeGrid(){
     gameContainer.removeChild(gameNodeList.item(gameNodeList.length-1));
 }
 
+makeGrid(gridSize);
+
 slider.addEventListener('input', () => {
     gridSize = slider.value;
     sliderText.textContent = `Grid Size: ${gridSize} x ${gridSize}`;
+    unmakeGrid();
+    makeGrid(gridSize);
+    normalButton.classList.add('active');
+    rainbowButton.classList.remove('active');
+    eraserButton.classList.remove('active');
 });
 
-let gridSize = slider.value;
-makeGrid(gridSize);
-
 clearButton.addEventListener('click', () => {
+    gridSize = slider.value;
     unmakeGrid();
+    makeGrid(gridSize);
+    normalButton.classList.add('active');
+    rainbowButton.classList.remove('active');
+    eraserButton.classList.remove('active');
+});
+
+normalButton.addEventListener('click', () => {
+    normalButton.classList.add('active');
+    rainbowButton.classList.remove('active');
+    eraserButton.classList.remove('active');
+    unmakeGrid();
+    makeGrid(gridSize);
+});
+
+rainbowButton.addEventListener('click', () => {
+    normalButton.classList.remove('active');
+    rainbowButton.classList.add('active');
+    eraserButton.classList.remove('active');
+    unmakeGrid();
+    makeGrid(gridSize);
+});
+
+eraserButton.addEventListener('click', () => {
+    normalButton.classList.remove('active');
+    rainbowButton.classList.remove('active');
+    eraserButton.classList.add('active');
+    let squares = this.grid.childNodes;
+    squares.forEach((square) => {
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = 'white';
+        });
+    });
 });
